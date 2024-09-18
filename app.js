2044bc6ea247f7b68fdc95b3da1ba516f2eafce1
vipulkumar82 <vipulkumar212003@gmail.com>
2024-09-18 21:02:33 +0530

    map added

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ const path = require('path');
 
 const app=express();
 const server=http.createServer(app);
-const io=socketio(server);
+const io = socketio(server);
 
-app.set("view engine",ejs);
-app.set(expres.static(path.join(__dirname,"public")))
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+
+io.on("connection",function(socket){
+    console.log("connected");
+})
 
 app.get('/',function (res,res){
-    res.send("Hellow World");
+    res.render("index");
 })
 
 server.listen(3000)
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,18 @@
 const socket = io();
 
-console.log();
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition((position)=>{
+        const { latitude, longitude } = position.coords;
+        socket.emit("send-location",{latitude,longitude});
+    }, (error) => {
+        console.log(error);
+    }, 
+    {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+    }
+);
+}
+
+L.map("map").setView([0,0], 10);
