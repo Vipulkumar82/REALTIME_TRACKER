eb77c59ef6d3245189e0f585adace952f6d7b5de
vipulkumar82 <vipulkumar212003@gmail.com>
2024-09-18 20:11:15 +0530

    added ejs css and js file and make linked with map

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const socketio=require("socket.io");
 const http=require("http");
+const path = require('path');
 
 const app=express();
 const server=http.createServer(app);
 const io=socketio(server);
+
+app.set("view engine",ejs);
+app.set(expres.static(path.join(__dirname,"public")))
 
 app.get('/',function (res,res){
     res.send("Hellow World");
